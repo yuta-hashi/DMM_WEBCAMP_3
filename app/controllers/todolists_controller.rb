e0eef70d4d8552ad7d683b78a222735abc488ac8
EC2 Default User <ec2-user@ip-172-31-78-216.ec2.internal>
2020-12-06 11:52:21 +0900

    [Start]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -36,7 +36,7 @@ class TodolistsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
   
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6cfa0c9160fd19b2fc0c024cbf87e9cd9dfd5260407cfbdfb44751cfd02546b3b08fd7c7998c830110ef743bf0e51e78b46881b810ca17366887b34613346cdb'
